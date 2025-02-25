a8a1bcc7a74ce38b838f6d22fbe202518206e6d1
Hosna Shams <hosna.s123@gmail.com>
2025-02-25 13:18:42 +0000

    Adds methods in Plotter class for visualise skew and methods to DataTransform class to correct skew with log transform or square root. Uses visualisation and skew in EDA notebook to identify columns that have skew and which methods to use to correct them.

--- transformations.py
+++ transformations.py
@@ -1,5 +1,9 @@
 import pandas as pd
-from db_utils import create_df
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy import stats
 
 class DataTransform:
     def __init__(self):
@@ -31,6 +35,23 @@ class DataTransform:
             dataframe[col] = dataframe[col].fillna(dataframe[col].mean())
         return(dataframe)
 
+    def correct_skew_log_transform(self, dataframe, *args):
+        for col in args:
+            dataframe.loc[:, col] = dataframe[col].map(lambda i: np.log(i) if i > 0 else 0.0)
+        return(dataframe)
+
+    def corect_skew_sqrt(self, dataframe, *args):   
+        for col in args:
+            dataframe[col] = dataframe[col].map(lambda i: np.sqrt(i))
+        return(dataframe)
+    
+    def correct_skew_yeo_johnson(self, dataframe, *args):
+        copy_df=dataframe.copy()
+        for col in args:
+          transformed, _ = stats.yeojohnson(copy_df[col])
+          copy_df[col] = transformed
+        return(copy_df)
+
 class DataFrameInfo:
     def __init__(self):
         pass
@@ -50,7 +71,17 @@ class DataFrameInfo:
             print(f'{col}: Mean={mean_value}, Median={median_value}, Mode={mode_value}, Standard deviation={std_value}')
     
     def categorical_distinct_values(self, dataframe):
-        col_list=['term', 'grade', 'sub_grade', 'employment_length', 'home_ownership', 'verification_status', 'loan_status', 'payment_plan', 'purpose', 'policy_code', 'application_type']
+        col_list=['term', 
+                  'grade', 
+                  'sub_grade', 
+                  'employment_length', 
+                  'home_ownership', 
+                  'verification_status', 
+                  'loan_status', 
+                  'payment_plan', 
+                  'purpose', 
+                  'policy_code', 
+                  'application_type']
         print('DataFrame count distinct values of categorical data:\n', dataframe[col_list].nunique())
     
     def df_shape(self, dataframe):
@@ -58,4 +89,51 @@ class DataFrameInfo:
     
     def df_null_percent(self, dataframe):
         print('DataFrame percentage of null values:\n', round(dataframe.isnull().sum()/len(dataframe)*100, 2))
+
+
+class Plotter:
+    def __init__(self):
+        pass
+
+    def plot_null(self, dataframe):
+        msno.bar(dataframe)
+    
+    def plot_all_skew_grid(self, dataframe): 
+        numeric_features = ['loan_amount',
+                    'funded_amount',
+                    'funded_amount_inv',
+                    'int_rate',
+                    'instalment',
+                    'annual_inc',
+                    'dti',
+                    'delinq_2yrs', 
+                    'inq_last_6mths', 
+                    'open_accounts', 
+                    'total_accounts', 
+                    'out_prncp', 
+                    'out_prncp_inv', 
+                    'total_payment', 
+                    'total_payment_inv', 
+                    'total_rec_prncp', 
+                    'total_rec_int', 
+                    'total_rec_late_fee', 
+                    'recoveries', 
+                    'collection_recovery_fee', 
+                    'last_payment_amount', 
+                    'collections_12_mths_ex_med']
         
+        def facet_hist(data, **kwargs):
+            ax = plt.gca()
+            # Plot the histogram with kde
+            sns.histplot(data=data, x='value', kde=True, **kwargs)
+            # Get skewness for the facet's data
+            skew_value = data['value'].skew()
+            # Annotate the facet with the skewness value
+            ax.text(0.7, 0.7, f"Skew: {skew_value:.2f}", 
+                    transform=ax.transAxes, fontsize=9,
+                    bbox=dict(facecolor='white', alpha=0.7))
+
+        f = pd.melt(dataframe, value_vars=numeric_features)
+        g = sns.FacetGrid(f, col="variable", col_wrap=3, sharex=False, sharey=False)
+        g.map_dataframe(facet_hist)
+        plt.show()
